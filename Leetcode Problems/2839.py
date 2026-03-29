ac7aff857d5c5079275bfe155ee34c7238a96254
Pranesh Jha <Praneshjha1038@gmail.com>
2026-03-29 22:16:58 +0530

    approach two

--- Leetcode Problems/2839.py
+++ Leetcode Problems/2839.py
@@ -1,13 +1,19 @@
-s1:str = "abcd"
-s2:str = "cdab"
+s1:str = "cmpr"
+s2:str = "rmcp"
 result:bool = True
-def equalString(s1, s2):
-    for i in range(2):
-        if s1[i] != s2[i]:
-            if s2[i+2] == s1[i]:
-                pass
-            else:
-                return False
-    return True
+# def equalString(s1, s2):
+#     for i in range(2):
+#         if s1[i] != s2[i]:
+#             if s2[i+2] == s1[i]:
+#                 pass
+#             else:
+#                 return False
+#     return True
 
-print(equalString(s1, s2))
+def canBeEqual(first:str, second:str) -> bool:
+    for i in range(4):
+        if s1[i] == s2[i]:
+            pass 
+        elif s1[i] != s2[(i + 2) % 4]:
+            return False
+    return True
