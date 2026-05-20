5540c020f82edb0ddb0edd1c7bb3a115fad28a05
Pranesh Jha <Praneshjha1038@gmail.com>
2026-05-20 23:57:13 +0530

    Day 55/90 90 Days Of DSA Challenge

---
+++ Leetcode Problems/2657.py
@@ -0,0 +1,16 @@
+A = [1,2,3]
+B = [3,1,2]
+n = len(A)
+res = [0] * n
+seen = [0] * (n + 1)
+
+for i in range(n):
+    seen[0] += seen[A[i]]
+    seen[A[i]] = 1
+    
+    seen[0] += seen[B[i]]
+    seen[B[i]] = 1
+    
+    res[i] = seen[0]
+    
+print(res)
